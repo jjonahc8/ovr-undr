60221abc4af548b290d2d016065f6cc6990a668e
c44rson <ccabre11@lion.lmu.edu>
2025-07-17 01:32:24 -0700

    starting right sidebar
    only searchbar for now

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,43 +1,33 @@
+import { BsSearch } from "react-icons/bs";
 import LeftSidebar from "./components/LeftSidebar";
+import MainComponent from "./components/MainComponent";
 
 const Home = () => {
   return (
     <div className="w-full h-full flex justify-center items-center relative bg-black">
-      <div className="max-w-screen-xl w-full h-full flex relative">
-        {/* Left Sidebar for navigation/header */}
+      <div className="max-w-[65vw] w-full h-full flex relative">
         <LeftSidebar />
-        <main
-          className="ml-[275px] flex w-[600px] h-full min-h-screen flex-col border-l-[0.5px] border-r-[0.5px] 
-        border-gray-600"
-        >
-          <h1 className="text-xl font-bold p-6">Home</h1>
-          <div
-            className="border-t-[0.5px] px-4 border-b-[0.5px] flex items-stretch py-4 space-x-2 border-gray-600
-          relative"
-          >
-            <div className="w-11 h-11 bg-slate-400 rounded-full flex-none"></div>
-            <div className="flex flex-col w-full h-full">
+        <MainComponent />
+        <section className="w-[30%] sticky top-0 flex flex-col items-stretch h-screen px-6">
+          <div>
+            <div className="relative w-full h-full">
+              <label
+                htmlFor="searchBox"
+                className="absolute top-0 left-0 h-full flex items-center justify-center"
+              >
+                <BsSearch className="w-5 h-5 text-gray-500" />
+              </label>
               <input
+                id="searchBox"
                 type="text"
-                className="w-full h-full text-2xl placeholder:text-gray-600 bg-transparent border-b-[0.5px] border-gray-600 p-4 
-                outline-none border-none"
-                placeholder="What's happening?"
+                placeholder="Search Twitter"
+                className="outline-none bg-transparent border-none w-full h-full rounded-xl py-4 px-8"
               />
-              <div className="w-full justify-between items-center flex">
-                <div></div>
-                <div className="w-full max-w-[100px]">
-                  <button
-                    className="rounded-full bg-primary px-4 py-2 w-full text-lg text-center hover:bg-primary/70 
-                  transition duration-200 font-bold"
-                  >
-                    Tweet
-                  </button>
-                </div>
-              </div>
             </div>
           </div>
-        </main>
-        {/* <section>right sidebar</section> */}
+          <div></div>
+          <div></div>
+        </section>
       </div>
     </div>
   );
