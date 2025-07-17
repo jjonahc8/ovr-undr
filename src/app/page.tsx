92f83adc61241ee06f246b624573ec43462dc777
c44rson <ccabre11@lion.lmu.edu>
2025-07-17 13:16:58 -0700

    non-functional css done

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,6 @@
-import { BsSearch } from "react-icons/bs";
 import LeftSidebar from "./components/LeftSidebar";
 import MainComponent from "./components/MainComponent";
+import RightSection from "./components/RightSection";
 
 const Home = () => {
   return (
@@ -8,26 +8,7 @@ const Home = () => {
       <div className="max-w-[65vw] w-full h-full flex relative">
         <LeftSidebar />
         <MainComponent />
-        <section className="w-[30%] sticky top-0 flex flex-col items-stretch h-screen px-6">
-          <div>
-            <div className="relative w-full h-full">
-              <label
-                htmlFor="searchBox"
-                className="absolute top-0 left-0 h-full flex items-center justify-center"
-              >
-                <BsSearch className="w-5 h-5 text-gray-500" />
-              </label>
-              <input
-                id="searchBox"
-                type="text"
-                placeholder="Search Twitter"
-                className="outline-none bg-transparent border-none w-full h-full rounded-xl py-4 px-8"
-              />
-            </div>
-          </div>
-          <div></div>
-          <div></div>
-        </section>
+        <RightSection />
       </div>
     </div>
   );
