ce18d0c844511f9b285ff19750a5153278fbc9d6
c44rson <ccabre11@lion.lmu.edu>
2025-07-25 13:19:18 -0700

    users forced to TL screen

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,19 @@
 import { AuthButton } from "@/components/auth-button";
 import Link from "next/link";
+import { createClient } from "@/lib/supabase/server";
+import { redirect } from "next/navigation";
 
-export default function Home() {
+export default async function Home() {
+  const supabase = await createClient();
+
+  const { data } = await supabase.auth.getClaims();
+
+  const user = data?.claims;
+
+  if (user) {
+    redirect("/protected");
+  }
+
   return (
     <main className="min-h-screen flex flex-col items-center">
       <div className="flex-1 w-full flex flex-col gap-20 items-center">
