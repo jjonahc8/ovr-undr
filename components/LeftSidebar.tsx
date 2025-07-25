a7c8dc9319cf6ccea53fb824076b5de12a7132ad
c44rson <ccabre11@lion.lmu.edu>
2025-07-24 17:36:37 -0700

    spacing fix

--- components/LeftSidebar.tsx
+++ components/LeftSidebar.tsx
@@ -54,7 +54,7 @@ export async function LeftSidebar() {
           className="rounded-full flex items-center space-x-2 bg-transparent p-4 text-center 
       hover:bg-white/10 transition duration-200 w-full justify-between"
         >
-          <div className="flex items-center space-x-3">
+          <div className="flex items-center space-x-2">
             <div className="rounded-full bg-slate-400 w-10 h-10"></div>
             <div className="text-left text-sm">
               {user?.user_metadata.display_name}
