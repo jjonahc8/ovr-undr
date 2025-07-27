8be38a98916f1c08062e0a86f6d2f2f7b2b19850
c44rson <ccabre11@lion.lmu.edu>
2025-07-27 02:39:14 -0700

    added async protection

--- app/protected/page.tsx
+++ app/protected/page.tsx
@@ -3,6 +3,7 @@ import { createClient } from "@/lib/supabase/server";
 import LeftSidebar from "@/components/LeftSidebar";
 import MainComponent from "@/components/MainComponent";
 import RightSection from "@/components/RightSection";
+import React from "react";
 
 export default async function ProtectedPage() {
   const supabase = await createClient();
@@ -16,7 +17,13 @@ export default async function ProtectedPage() {
     <div className="w-full h-full flex justify-center text-white items-center relative bg-black">
       <div className="max-w-[80vw] w-full h-full flex relative">
         <LeftSidebar />
-        <MainComponent />
+        {(async () => {
+          const mainComponentResult = await MainComponent();
+          if (React.isValidElement(mainComponentResult)) {
+            return mainComponentResult;
+          }
+          return <div>Error loading timeline</div>;
+        })()}
         <RightSection />
       </div>
     </div>
