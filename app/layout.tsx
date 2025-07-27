67c2615a36b7d8f8c34d5b0829baa1dfc577dad2
c44rson <ccabre11@lion.lmu.edu>
2025-07-27 02:38:59 -0700

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -24,7 +24,7 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en" suppressHydrationWarning>
+    <html lang="en">
       <body className={`${geistSans.className} antialiased`}>{children}</body>
     </html>
   );
