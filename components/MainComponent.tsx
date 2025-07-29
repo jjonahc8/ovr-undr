e97787d1761abc308c038fd2a4351cdede63ce28
c44rson <ccabre11@lion.lmu.edu>
2025-07-28 21:07:45 -0700

    Update MainComponent.tsx

--- components/MainComponent.tsx
+++ components/MainComponent.tsx
@@ -18,34 +18,6 @@ const MainComponent = async function () {
 
   const timelineLength = tweets?.length;
 
-  function generateDate(supabaseDate: any): any {
-    const nowDate: Date = new Date();
-    const postDate: Date = parseISO(supabaseDate);
-    const timeDifferenceInMilliseconds: number =
-      nowDate.getTime() - postDate.getTime();
-
-    const seconds = Math.floor(timeDifferenceInMilliseconds / 1000);
-    const minutes = Math.floor(seconds / 60);
-    const hours = Math.floor(minutes / 60);
-    const days = Math.floor(hours / 24);
-    const months = Math.floor(days / 30.44);
-    const years = Math.floor(days / 365.25);
-
-    if (seconds < 60) {
-      return `${seconds} seconds ago`;
-    } else if (minutes < 60) {
-      return `${minutes} minute${minutes === 1 ? "" : "s"} ago`;
-    } else if (hours < 24) {
-      return `${hours} hour${hours === 1 ? "" : "s"} ago`;
-    } else if (days < 30) {
-      return `${days} day${days === 1 ? "" : "s"} ago`;
-    } else if (months < 12) {
-      return `${months} month${months === 1 ? "" : "s"} ago`;
-    } else {
-      return `${years} year${years === 1 ? "" : "s"} ago`;
-    }
-  }
-
   // need to include case where there are no tweets available
   if (timelineLength) {
     return (
