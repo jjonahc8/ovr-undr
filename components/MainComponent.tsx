8cd94f87b56924d1b89b8042152e5f41b206d350
c44rson <ccabre11@lion.lmu.edu>
2025-07-28 21:51:32 -0700

    fixed top blur

--- components/MainComponent.tsx
+++ components/MainComponent.tsx
@@ -21,10 +21,10 @@ const MainComponent = async function () {
   // need to include case where there are no tweets available
   if (timelineLength) {
     return (
-      <main className="sticky top-0 flex w-[55%] h-full min-h-screen flex-col border-l-[0.5px] border-r-[0.5px] border-gray-600">
-        <h1 className="text-xl font-bold ml-6 mt-6 mb-4 backdrop-blur bg-black/10 sticky top-0">
-          Home
-        </h1>
+      <main className="sticky top-0 flex w-[45%] h-full min-h-screen flex-col border-l-[0.5px] border-r-[0.5px] border-gray-600">
+        <div className="backdrop-blur sticky top-0">
+          <h1 className="text-xl font-bold ml-6 mt-6 mb-4">Home</h1>
+        </div>
         <div
           className="border-t-[0.5px] px-4 border-b-[0.5px] flex items-stretch py-4 border-gray-600
     relative"
